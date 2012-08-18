8c0059bb59e03190b12096748b9934d4ff3f8740
Gaurav Rawat <gauravbrills14@gmail.com>
2012-08-18 12:08:19 +0530

    First cut

---
+++ src/play/gauravbrills/connectfour/A1Application.java
@@ -0,0 +1,14 @@
+package play.gauravbrills.connectfour;
+/*
+ * Purpose: This is the application class for Asst 1 
+ * You do not need to modify this class
+ *
+ * Author:  
+ * Date:    Semester 2, 2012
+ */
+public class A1Application {
+    public static void main(String[] args) {
+        A1Program assignmentOne = new A1Program();
+        assignmentOne.start();
+    }
+}
